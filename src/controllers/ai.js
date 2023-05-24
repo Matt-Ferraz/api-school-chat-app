c084f242bfd2ef2f1982b84dc1c48d077cddc149
Matt-Ferraz <helinhorx2016@gmail.com>
2023-05-24 17:10:46 -0300

    Updated async flow

--- src/controllers/ai.js
+++ src/controllers/ai.js
@@ -1,5 +1,6 @@
 import { Configuration, OpenAIApi } from "openai";
 import dotenv from "dotenv";
+import { parse } from "dotenv";
 dotenv.config();
 
 const apiKey = process.env.OPEN_AI_API_KEY;
@@ -11,18 +12,23 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export async function askAi(input) {
+    input = `${input}. Return response in this format \n {"question": "Por que a galinha atravessou a rua", "response": "para chegar do outro lado"}`;
     try {
-        const response = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: input,
-            temperature: 1,
-            max_tokens: 2048,
-        });
-        const answer = response.data.choices[0].text.trim();
-        console.log(answer);
-        return answer;
+        await openai
+            .createCompletion({
+                model: "text-davinci-003",
+                prompt: input,
+                temperature: 1,
+                max_tokens: 2000,
+            })
+            .then((res) => {
+                const answer = res.data.choices[0].text.trim();
+                return answer;
+            });
     } catch (error) {
-        console.error("OpenAI API error:", error.response.data);
+        console.error("OpenAI API error:", error);
+        if (error.toJSON().status > 500 && error.toJSON().status < 600)
+            return console.log(error);
         return error;
     }
 }
